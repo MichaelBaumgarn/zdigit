a69344383ef7cd6404862b04a46e23ecbe0bca98
michaelbaumgarn <michael.baumgarn@gmail.com>
2022-12-07 13:15:07 +0100

    add test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
 
-test('renders learn react link', () => {
+import App from "./App";
+import React from "react";
+import userEvent from "@testing-library/user-event";
+
+test("common app use cases", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/customer/i);
   expect(linkElement).toBeInTheDocument();
+
+  const searchInput = screen.getByTestId("search-input");
+  userEvent.type(searchInput, "0234");
+  const firstRow = screen.getByTestId("col-customer-0");
+  expect(firstRow.textContent).toBe("Hansen-Block AG");
 });
